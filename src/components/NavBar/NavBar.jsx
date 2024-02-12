d9db10c84b1dc9a70b6c8adb4cf1e468a4db0b5d
Haseeb Althaf <haseebalthaf1@outlook.com>
2024-02-12 20:54:34 +0530

    Second Commit.

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import "./NavBar.css";
+
+function NavBar() {
+    return(
+        <>
+        <div className="navBar">
+            <img src="images/logo.png" alt="website-logo" className="logoContainer" height="45" />
+            <nav className="navContainer">
+                <a className="navItem active" href="index.html">Home</a>
+                <a className="navItem" href="about.html">About</a>
+                <a className="navItem" href="blog.html">Blog</a>
+            </nav>
+        </div>
+        </>
+    );
+}
+
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar/NavBar";
+import "../main.css";
+
+function Home() {
+    return(
+        <>
+            <NavBar />
+        </>
+    );
+}
+
+export default Home;
