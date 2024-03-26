e588a9580126abf72347217aad5dabca11f0c1f6
Haseeb Althaf <haseebalthaf1@outlook.com>
2024-03-26 05:40:44 +0530

    Added new projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,12 +14,37 @@ function Projects() {
       <Footer />
       <div className="projectContainer">
         <ul>
-          <li key="portfolio">
-            Portfolio - A website about myself for Advanced Client Side<br />
-            <time dateTime="2023-11-14">14th November 2023</time>
-          </li>
           <li key="groupProject">
-            Software Development Group Project - (TBD)
+            ALiyah e-Learning Platform
+            <p>Assisted in creating an e-Learning Platform using MERN stack for Software Development Group Project module.</p>
+          </li>
+          <li key="individualProject">
+            Real Estate Website
+            <p>Created a website for real estate displaying properties using ReactJS, CSS and Bootstrap for Advanced Client Side module.</p>
+          </li>
+          <li key="individualProject">
+            Westminster Shopping Manager
+            <p>Created a command line based shopping manager using Java for Object Oriented Programming module.</p>
+          </li>
+          <li key="individualProject">
+            Restaurant Billing System
+            <p>Created a command line based billing system for a restaurant using Java for Software Development 2 module.</p>
+          </li>
+          <li key="individualProject">
+            University Grading System
+            <p>Created a command line based grading system for university using Python for Software Development 1 module.</p>
+          </li>
+          <li key="individualProject">
+            University Grading System
+            <p>Created a command line based grading system for university using Python for Software Development 1 module.</p>
+          </li>
+          <li key="individualProject">
+            Sports Club Website
+            <p>Assisted in creating a Sports based information/shopping website using HTML, CSS and JavaScript.</p>
+          </li>
+          <li key="portfolio">
+            Portfolio - A website about myself for Advanced Client Side
+            <p>Created a portfolio website about myself using ReactJS and CSS.</p>
           </li>
         </ul>
       </div>
